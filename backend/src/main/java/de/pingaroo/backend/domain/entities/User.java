d0b5b0e98a2b04591d54915b99a83dd3c95d96ef
Nils Zentgraf <nils.zentgraf@aol.com>
2025-05-04 20:36:05 +0200

    feat: add isActive field to User entity and update equals/hashCode methods

--- backend/src/main/java/de/pingaroo/backend/domain/entities/User.java
+++ backend/src/main/java/de/pingaroo/backend/domain/entities/User.java
@@ -41,18 +41,21 @@ public class User {
     private Role role;
     
     @Column(nullable = false)
+    private boolean isActive;
+    
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(apiEndpoints, user.apiEndpoints) && role == user.role && Objects.equals(createdAt, user.createdAt);
+        return isActive == user.isActive && Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(apiEndpoints, user.apiEndpoints) && role == user.role && Objects.equals(createdAt, user.createdAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, email, password, firstName, lastName, apiEndpoints, role, createdAt);
+        return Objects.hash(id, email, password, firstName, lastName, apiEndpoints, role, isActive, createdAt);
     }
 
     @PrePersist
